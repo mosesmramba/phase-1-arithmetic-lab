edde321caa6c812d1506bf27ea4710e38916ba01
mosesmramba <moses.mramba@student.moringaschool.com>
2023-09-19 15:19:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1=62;
+let num2=1;
+const multiply=num1*num2;
+
+const random=7
+Math.random();
+
+let num3=24;
+let num4=5;
+const mod=num3%num4;
+
+const max=20
+
